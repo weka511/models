2f8edc992abab0acfa08e554ed37dfbbce9fcb2e
Simon Crase <simon@greenweaves.nz>
2023-02-10 16:00:44 +1300

    #1 Place People on grid

--- schelling.py
+++ schelling.py
@@ -1,24 +1,84 @@
+#!/usr/bin/env python
+
 # https://mesa.readthedocs.io/en/stable/tutorials/intro_tutorial.html
 
-from mesa      import Agent, Model
-from mesa.time import RandomActivation
+'''
+Schelling Segregation model in mesa
+'''
+
+from abc               import abstractmethod
+from matplotlib.pyplot import colorbar, imshow, show
+from mesa              import Agent, Model
+from mesa.space        import MultiGrid
+from mesa.time         import RandomActivation
+from numpy             import zeros
 
 class Person(Agent):
+
     def __init__(self, unique_id, model):
-        super().__init__(unique_id, model)(self)
+        super().__init__(unique_id, model)
+
+    @abstractmethod
     def step(self):
-        pass
+        ...
+    @abstractmethod
+    def get_indicator(self):
+        ...
+
+class Red(Person):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+    def step(self):
+        print (f'{self.unique_id} Red')
+
+    def get_indicator(self):
+        return 1
+
+class Blue(Person):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
+    def step(self):
+        print (f'{self.unique_id} Blue')
+
+    def get_indicator(self):
+        return 2
 
 class SchellingModel(Model):
 
-    def __init__(self, N):
+    def __init__(self, width=10, height=10,nRed=75,nBlue=10):
         self.schedule = RandomActivation(self)
-        for i in range(N):
-            self.schedule.add(Person(i, self))
+        self.grid     = MultiGrid(width, height, True)
+        for i in range(nRed):
+            self.place(Red(i, self))
+        for i in range(nBlue):
+            self.place(Blue(i+nRed, self))
+
+    def place(self,person):
+        x = self.random.randrange(self.grid.width)
+        y = self.random.randrange(self.grid.height)
+        while len(self.grid.get_cell_list_contents((x,y)))>0:
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+        self.grid.place_agent(person, (x, y))
+        self.schedule.add(person)
 
     def step(self):
         self.schedule.step()
 
+    def get_counts(self):
+        agent_counts = zeros((self.grid.width, self.grid.height))
+        for cell in self.grid.coord_iter():
+            cell_content, x, y = cell
+            for person in cell_content:
+                agent_counts[x][y] = person.get_indicator()
+        return agent_counts
+
 if __name__ == '__main__':
-    schelling_model = SchellingModel(10)
+    model = SchellingModel()
     model.step()
+
+    imshow(model.get_counts(), interpolation="nearest")
+    colorbar()
+    show()
