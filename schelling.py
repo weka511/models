7fe3b632991fd0d8e260edb8008a4f2dd3b361db
Simon Crase <simon@greenweaves.nz>
2023-02-10 19:38:36 +1300

    #1 First working version

--- schelling.py
+++ schelling.py
@@ -7,7 +7,7 @@ Schelling Segregation model in mesa
 '''
 
 from abc               import abstractmethod
-from matplotlib.pyplot import colorbar, imshow, show
+from matplotlib.pyplot import colorbar, figure, imshow, show
 from mesa              import Agent, Model
 from mesa.space        import MultiGrid
 from mesa.time         import RandomActivation
@@ -21,16 +21,25 @@ class Person(Agent):
     @abstractmethod
     def step(self):
         ...
+
     @abstractmethod
     def get_indicator(self):
         ...
 
+    def step(self):
+        n_like_me, n_different = self.count_neighbours()
+        if n_like_me==0 or n_like_me/(n_like_me+n_different)<self.model.limit:
+            self.model.move(self)
+
+    def count_neighbours(self):
+        return self.model.count_neighbours(self,self.pos)
+
+    def __str__(self):
+        return f'{self.unique_id} {self.get_indicator()} {self.pos}'
+
 class Red(Person):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-
-    def step(self):
-        print (f'{self.unique_id} Red')
 
     def get_indicator(self):
         return 1
@@ -39,21 +48,25 @@ class Blue(Person):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
 
-    def step(self):
-        print (f'{self.unique_id} Blue')
-
     def get_indicator(self):
         return 2
 
 class SchellingModel(Model):
 
-    def __init__(self, width=10, height=10,nRed=75,nBlue=10):
+    def __init__(self,
+                 width  = 10,
+                 height = 10,
+                 nRed   = 75,
+                 nBlue  = 10,
+                 limit  = 0.25):
         self.schedule = RandomActivation(self)
         self.grid     = MultiGrid(width, height, True)
+        self.limit    = limit
         for i in range(nRed):
             self.place(Red(i, self))
         for i in range(nBlue):
             self.place(Blue(i+nRed, self))
+        self.empty = [(x,y) for x in range(width) for y in range(height) if len(self.grid.get_cell_list_contents((x,y)))==0]
 
     def place(self,person):
         x = self.random.randrange(self.grid.width)
@@ -75,10 +88,42 @@ class SchellingModel(Model):
                 agent_counts[x][y] = person.get_indicator()
         return agent_counts
 
+    def move(self,person):
+        pos = self.find_candidate(person)
+        print (f'Move {person} to {pos}')
+        if pos!=None:
+            self.empty.remove(pos)
+            self.empty.append(person.pos)
+            self.grid.move_agent(person, pos)
+
+    def find_candidate(self,person):
+        for pos in self.empty:
+            n_like_me, n_different = self.count_neighbours(person,pos)
+            if n_like_me>self.limit *(n_like_me+n_different):
+                return pos
+
+    def count_neighbours(self,person,pos):
+        n_like_me   = 0
+        n_different = 0
+        for neighbour in self.grid.iter_neighbors(pos, False):
+            if neighbour.get_indicator()==person.get_indicator():
+                n_like_me += 1
+            else:
+                n_different += 1
+
+        return n_like_me, n_different
+
 if __name__ == '__main__':
     model = SchellingModel()
-    model.step()
 
+    figure()
     imshow(model.get_counts(), interpolation="nearest")
     colorbar()
+
+    for i in range(25):
+        model.step()
+    figure()
+    imshow(model.get_counts(), interpolation="nearest")
+    colorbar()
+
     show()
